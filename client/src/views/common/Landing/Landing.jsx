a4325351d5aac55ade08d3b70494ecee5cf4af05
Manuel Miglioranza <manuel.miglioranza@zenvia.com>
2022-08-02 23:12:05 -0300

    Landing view skeleton

---
+++ client/src/views/common/Landing/Landing.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Landing = props => {
+  return (
+    <div>Landing</div>
+  )
+}
+
+Landing.propTypes = {
+    props: PropTypes.object.isRequired
+}
+
+export default Landing
