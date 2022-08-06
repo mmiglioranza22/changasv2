4e8a6955095299e9141cbfe99111e208accdb8fd
Manuel Miglioranza <manuel.miglioranza@zenvia.com>
2022-08-06 15:56:10 -0300

    Color pallete draft

--- client/src/index.js
+++ client/src/index.js
@@ -5,12 +5,16 @@ import App from './App'
 import reportWebVitals from './reportWebVitals'
 import store from './redux/store'
 import { Provider } from 'react-redux'
+import { ThemeProvider } from '@mui/material/styles'
+import theme from './theme'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>
 )
--- client/src/views/common/Landing/Landing.jsx
+++ client/src/views/common/Landing/Landing.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 // import PropTypes from 'prop-types'
+// import { useTheme } from '@mui/material/styles'
 
+// eslint-disable-next-line no-unused-vars
 const Landing = props => {
   return (
-    <div>Landing {props}</div>
+    <div>Landing </div>
   )
 }
 
