c6933244e784c7ffc60be5c1af031fc2efc39e04
Manuel Miglioranza <manuel.miglioranza@zenvia.com>
2022-08-06 23:42:44 -0300

    wip testing css modules hierarchy

--- client/src/views/common/Landing/Landing.jsx
+++ client/src/views/common/Landing/Landing.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 // import PropTypes from 'prop-types'
 // import { useTheme } from '@mui/material/styles'
 import { styled } from '@mui/material/styles'
+import styles from './Landing.module.css'
 
 import Button from '@mui/material/Button'
 
@@ -11,10 +12,15 @@ const landingStyles = {
 const MyComponent = styled('div')({
   backgroundColor: 'blue'
 })
+// create color gradient
+// center div with logo
+// nice typo for Changas
+// make animation *appears on face, to the right. more text on left appears
+// markup for signup template
 // eslint-disable-next-line no-unused-vars
 const Landing = props => {
   return (
-    <MyComponent>Landing
+    <MyComponent className={styles.root}>Landing
     <Button>hola</Button>
 
     </MyComponent>
