9689e6104051da9442d2e4659df5a42a68566258
Manuel Miglioranza <manuel.miglioranza@zenvia.com>
2022-08-06 22:26:35 -0300

    mui system install, wip

--- client/src/views/common/Landing/Landing.jsx
+++ client/src/views/common/Landing/Landing.jsx
@@ -1,20 +1,27 @@
 import React from 'react'
 // import PropTypes from 'prop-types'
 // import { useTheme } from '@mui/material/styles'
+import { styled } from '@mui/material/styles'
 
 import Button from '@mui/material/Button'
 
+const landingStyles = {
+  backgroundColor: 'red'
+}
+const MyComponent = styled('div')({
+  backgroundColor: 'blue'
+})
 // eslint-disable-next-line no-unused-vars
 const Landing = props => {
   return (
-    <div>Landing
+    <MyComponent>Landing
     <Button>hola</Button>
 
-    </div>
+    </MyComponent>
   )
 }
 
 Landing.propTypes = {
 }
 
-export default Landing
+export default styled(Landing)(landingStyles)
